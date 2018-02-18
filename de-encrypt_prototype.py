05679ded641277df995065818d23a20d318e0f7c
ArcKnight18 <36544247+ArcKnight18@users.noreply.github.com>
2018-02-17 23:36:14 -0500

    Update de-encrypt_prototype.py

--- de-encrypt_prototype.py
+++ de-encrypt_prototype.py
@@ -1,5 +1,5 @@
 # Encryption/Decryption test Original version by Symb1ote and ArcKnight18
-# Last Modified on: 2/17/2018, 11:19 PM
+# Last Modified on: 2/17/2018, 11:34 PM
 
 # Encrypt Test
 
@@ -15,21 +15,23 @@ sentence = input("Message:")
 
 for i in range(0,len(sentence)):
   asciiNum = (ord(sentence[i])) 
-  text = str(bin(asciiNum)[2:])  
+  text = str(bin(asciiNum)[2:])
+  
   for tex in range(0,len(text)):
     asciiB= (ord(text[tex])) 
     numZeroes= random.randint(97,122)
     numOnes= random.randint(65,90)
+    
     if asciiB == 48:
       randLettZero= chr(numZeroes)
       letterHoriz += randLettZero
+      
     elif asciiB == 49:
       randLettOne = chr(numOnes) 
       letterHoriz += randLettOne
 print ("Encrypted Message:",letterHoriz)
 
 # Decrypt Test
-
 countSort = 0
 groupBin = ""
 realBin = ""
@@ -38,12 +40,15 @@ horizontal = 0
 
 for D in range(0,len(letterHoriz)):
   asciiValue = (ord(letterHoriz[D]))
+  
   if 122 >= asciiValue >= 97:
     countSort += 1
     groupBin += "0"
+    
   elif 90 >= asciiValue >= 65:
     countSort += 1
     groupBin += "1"
+    
   if countSort in (7, countRep):
     realBin = ("" + groupBin) # Altered
     horzant = (chr(int(realBin, 2)))
